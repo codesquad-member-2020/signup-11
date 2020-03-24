0dbc7d9add59d5fcc79cc8b5525f087426174367
Han <quaint3@gmail.com>
2020-03-24 16:00:29 +0900

    [chore] users , interests table schema, data
    users table, interests table , data 설정 파일 생성

---
+++ BE/signup-server/src/main/resources/data.sql
@@ -0,0 +1,3 @@
+INSERT INTO users (seq,id,password,email,name,birth,phone) VALUES (null, 'test', 'test', 'test@gmail.com', '테스터', '2020-03-22', '010-1234-5678');
+
+INSERT INTO interests (seq, user_seq, contents) VALUES ( null, 1, '영화, 자전거');
---
+++ BE/signup-server/src/main/resources/schema.sql
@@ -0,0 +1,25 @@
+DROP TABLE IF EXISTS users CASCADE;
+
+CREATE TABLE users
+(
+    seq       bigint NOT NULL AUTO_INCREMENT,
+    id        varchar(25),
+    password  varchar(80),
+    email     varchar(50),
+    name      varchar(10),
+    birth     date,
+    phone     varchar(20),
+    create_at datetime NOT NULL DEFAULT CURRENT_TIMESTAMP(),
+    PRIMARY KEY (seq),
+    CONSTRAINT unq_user_id UNIQUE (id)
+);
+
+CREATE TABLE interests
+(
+    seq       bigint NOT NULL AUTO_INCREMENT,
+    user_seq  bigint NOT NULL ,
+    contents  varchar(100),
+    create_at datetime NOT NULL DEFAULT CURRENT_TIMESTAMP(),
+    PRIMARY KEY (seq),
+    CONSTRAINT fk_interest_to_user FOREIGN KEY (user_seq) REFERENCES users (seq) ON DELETE RESTRICT ON UPDATE  RESTRICT
+);
