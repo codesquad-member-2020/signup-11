67944bb9dc6e0f29e2bedd190ceb8ce3b5ad2976
Han <quaint3@gmail.com>
2020-03-24 22:20:03 +0900

    [feat] UserResController 생성, /api/users 처리
    해당 url로 POST 요청이 왔을 때, User Service를 거쳐 repository 저장 하도록 설정하고
    DB 에 제대로 저장이 되는 지 로그로 확인
    이제 result를 어떻게 처리할지 고민하면 되지 않을까.

---
+++ BE/signup-server/src/main/java/com/signup11/be/controller/UserRestController.java
@@ -0,0 +1,35 @@
+package com.signup11.be.controller;
+
+import com.signup11.be.model.JoinRequest;
+import com.signup11.be.model.User;
+import com.signup11.be.service.UserService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("api")
+public class UserRestController {
+
+  private final UserService userService;
+
+  private Logger logger = LoggerFactory.getLogger(UserRestController.class);
+
+  public UserRestController(UserService userService) {
+    this.userService = userService;
+  }
+
+  @PostMapping(path = "users")
+  public void join(@RequestBody JoinRequest joinRequest) {
+    logger.debug("joinRequest mapping {}", joinRequest);
+    User user = userService.join(
+        joinRequest.getUserId(),
+        joinRequest.getPassword(),
+        joinRequest.getName()
+    );
+    logger.debug("saved User {},",user);
+  }
+}
