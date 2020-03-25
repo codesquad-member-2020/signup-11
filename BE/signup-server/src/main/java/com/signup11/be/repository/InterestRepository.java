c2270893bba440124c40b3acd54953b19bd09c3c
Han <quaint3@gmail.com>
2020-03-25 16:04:00 +0900

    [feat] interest findby userSeq mehod 추가
    유저의 seq로 관심사를 찾는 쿼리문을 추가

--- BE/signup-server/src/main/java/com/signup11/be/repository/InterestRepository.java
+++ BE/signup-server/src/main/java/com/signup11/be/repository/InterestRepository.java
@@ -1,8 +1,12 @@
 package com.signup11.be.repository;
 
 import com.signup11.be.model.Interest;
+import java.util.List;
+import org.springframework.data.jdbc.repository.query.Query;
 import org.springframework.data.repository.CrudRepository;
 
 public interface InterestRepository extends CrudRepository<Interest, Long> {
 
+  @Query("SELECT * FROM interests i WHERE i.user_seq = :userSeq")
+  List<Interest> findByUserSeq(Long userSeq);
 }
