3caf6616426fe9f75eba0b097a1b377b62e4ff1e
ehgud0670 <kimdo2297@gmail.com>
2020-03-25 20:45:54 +0900

    [feat] 텍스트 필드의 테두리 두께을 더 얆게 하고 회색이 되도록 기능 구현함

--- iOS/SignupApp/SignupApp/Views/TextField/CustomTextField.swift
+++ iOS/SignupApp/SignupApp/Views/TextField/CustomTextField.swift
@@ -8,6 +8,24 @@
 
 import UIKit
 
+extension UIColor {
+    convenience init?(red: Int, green: Int, blue: Int) {
+        guard red >= 0 && red <= 255,
+            green >= 0 && green <= 255,
+            blue >= 0 && blue <= 255
+            else { return nil }
+        self.init(red: CGFloat(red) / 255.0, green: CGFloat(green) / 255.0, blue: CGFloat(blue) / 255.0, alpha: 1.0)
+    }
+    
+    convenience init?(rgb: Int) {
+        self.init(
+            red: (rgb >> 16) & 0xFF,
+            green: (rgb >> 8) & 0xFF,
+            blue: rgb & 0xFF
+        )
+    }
+}
+
 class CustomTextField: UITextField {
     
     override init(frame: CGRect) {
@@ -21,6 +39,8 @@ class CustomTextField: UITextField {
     }
     
     private func setup() {
-        
+        layer.borderWidth = 0.9
+        let greyColor = UIColor(rgb: 0x707070)!
+        layer.borderColor = greyColor.cgColor
     }
 }
