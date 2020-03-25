df69bc6c61688c1af4831604637663ab8a407827
Han <quaint3@gmail.com>
2020-03-25 14:00:15 +0900

    [chore] data,schema.sql 수정
    아이디, 패스워드, 이름만으로 회원가입 되던 것에서, 다른 항목들을 추가하여 테스트하기 위해 수정함.

--- BE/signup-server/src/main/resources/schema.sql
+++ BE/signup-server/src/main/resources/schema.sql
@@ -6,9 +6,10 @@ CREATE TABLE users
     id        varchar(25),
     password  varchar(80),
     name      varchar(10),
---     email     varchar(50),
---     birth     date,
---     phone     varchar(20),
+    birth     date,
+    gender    varchar (10),
+    email     varchar(50),
+    phone     varchar(20),
     create_at datetime NOT NULL DEFAULT CURRENT_TIMESTAMP(),
     PRIMARY KEY (seq),
     CONSTRAINT unq_user_id UNIQUE (id)
@@ -18,7 +19,7 @@ CREATE TABLE interests
 (
     seq       bigint NOT NULL AUTO_INCREMENT,
     user_seq  bigint NOT NULL ,
-    contents  varchar(100),
+    content   varchar(25),
     create_at datetime NOT NULL DEFAULT CURRENT_TIMESTAMP(),
     PRIMARY KEY (seq),
     CONSTRAINT fk_interest_to_user FOREIGN KEY (user_seq) REFERENCES users (seq) ON DELETE RESTRICT ON UPDATE  RESTRICT
