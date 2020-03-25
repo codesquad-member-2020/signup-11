e0d8266f02627fd82fc2bd961f691871852e6f78
Han <quaint3@gmail.com>
2020-03-25 14:01:47 +0900

    [feat] InterestRepository 생성
    Interest DAO 객체, 현재는 저장만 할 예정이므로 다른 메서드를 정의하지는 않았음.

---
+++ BE/signup-server/src/main/java/com/signup11/be/repository/InterestRepository.java
@@ -0,0 +1,8 @@
+package com.signup11.be.repository;
+
+import com.signup11.be.model.Interest;
+import org.springframework.data.repository.CrudRepository;
+
+public interface InterestRepository extends CrudRepository<Interest, Long> {
+
+}
