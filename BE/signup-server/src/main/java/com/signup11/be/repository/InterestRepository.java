8c3f3fc8b1937b49cfd4f20f2dc048af9f040f0f
Han <quaint3@gmail.com>
2020-03-25 16:44:35 +0900

    [refactor] @Param 추가
    Query와 매칭을 명확하게 보여주기 위해서 추가했음

--- BE/signup-server/src/main/java/com/signup11/be/repository/InterestRepository.java
+++ BE/signup-server/src/main/java/com/signup11/be/repository/InterestRepository.java
@@ -4,9 +4,10 @@ import com.signup11.be.model.Interest;
 import java.util.List;
 import org.springframework.data.jdbc.repository.query.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 
 public interface InterestRepository extends CrudRepository<Interest, Long> {
 
   @Query("SELECT * FROM interests i WHERE i.user_seq = :userSeq")
-  List<Interest> findByUserSeq(Long userSeq);
+  List<Interest> findByUserSeq(@Param(value = "userSeq") Long userSeq);
 }
