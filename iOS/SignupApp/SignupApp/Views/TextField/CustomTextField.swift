e2602c95e374073d0f680a36d8bda535279c0492
ehgud0670 <kimdo2297@gmail.com>
2020-03-26 16:11:04 +0900

    [chore] UIColor 위치 파일 맨 위로 옮김 이유 : 쓰이는 곳 위에 있게 하려고

--- iOS/SignupApp/SignupApp/Views/TextField/CustomTextField.swift
+++ iOS/SignupApp/SignupApp/Views/TextField/CustomTextField.swift
@@ -8,6 +8,27 @@
 
 import UIKit
 
+extension UIColor {
+    convenience init?(red: Int, green: Int, blue: Int) {
+        guard red >= 0 && red <= 255,
+            green >= 0 && green <= 255,
+            blue >= 0 && blue <= 255
+            else { return nil }
+        self.init(red: CGFloat(red) / 255.0,
+                  green: CGFloat(green) / 255.0,
+                  blue: CGFloat(blue) / 255.0,
+                  alpha: 1.0)
+    }
+    
+    convenience init?(rgb: Int) {
+        self.init(
+            red: (rgb >> 16) & 0xFF,
+            green: (rgb >> 8) & 0xFF,
+            blue: rgb & 0xFF
+        )
+    }
+}
+
 class CustomTextField: UITextField {
     override init(frame: CGRect) {
         super.init(frame: frame)
@@ -39,26 +60,5 @@ extension CustomTextField {
     
     override func editingRect(forBounds bounds: CGRect) -> CGRect {
         return bounds.inset(by: CustomTextField.padding)
-    }
-}
-
-extension UIColor {
-    convenience init?(red: Int, green: Int, blue: Int) {
-        guard red >= 0 && red <= 255,
-            green >= 0 && green <= 255,
-            blue >= 0 && blue <= 255
-            else { return nil }
-        self.init(red: CGFloat(red) / 255.0,
-                  green: CGFloat(green) / 255.0,
-                  blue: CGFloat(blue) / 255.0,
-                  alpha: 1.0)
-    }
-    
-    convenience init?(rgb: Int) {
-        self.init(
-            red: (rgb >> 16) & 0xFF,
-            green: (rgb >> 8) & 0xFF,
-            blue: rgb & 0xFF
-        )
     }
 }
