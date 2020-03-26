1e339250ab340a13bcca1c36196ae18e7ca4aa67
Han <quaint3@gmail.com>
2020-03-26 22:50:56 +0900

    [feat] 로그인 요청 객체 생성
    @RequestBody로 받기 위해서, 객체 생성

---
+++ BE/signup-server/src/main/java/com/signup11/be/model/AuthenticationRequest.java
@@ -0,0 +1,19 @@
+package com.signup11.be.model;
+
+public class AuthenticationRequest {
+
+  private String userId;
+
+  private String password;
+
+  public AuthenticationRequest() {
+  }
+
+  public String getUserId() {
+    return userId;
+  }
+
+  public String getPassword() {
+    return password;
+  }
+}
