c1b006d09e03a14d2a41b0bd40a8914e4832b44d
Han <quaint3@gmail.com>
2020-03-26 22:57:24 +0900

    [feat] login, get 추가
    login 기능, 회원정보조회 기능 추가

--- BE/signup-server/src/main/java/com/signup11/be/controller/UserRestController.java
+++ BE/signup-server/src/main/java/com/signup11/be/controller/UserRestController.java
@@ -1,12 +1,20 @@
 package com.signup11.be.controller;
 
 import com.signup11.be.api.ApiResult;
+import com.signup11.be.error.UnauthorizedException;
+import com.signup11.be.error.UserNotFoundException;
+import com.signup11.be.model.AuthenticationRequest;
 import com.signup11.be.model.Interest;
 import com.signup11.be.model.JoinRequest;
 import com.signup11.be.model.JoinResult;
 import com.signup11.be.model.User;
 import com.signup11.be.service.UserService;
 import java.util.List;
+import java.util.Optional;
+import javax.servlet.http.HttpSession;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -38,5 +46,26 @@ public class UserRestController {
     List<Interest> interestList = userService.findByUserSeq(saved.getSeq());
 
     return ApiResult.OK(new JoinResult(saved, interestList));
+  }
+
+  @PostMapping(path = "login")
+  public ApiResult<Boolean> login(@RequestBody AuthenticationRequest request, HttpSession session) {
+    String userId = request.getUserId();
+    String password = request.getPassword();
+    User user = userService.login(userId, password);
+    session.setAttribute(user.getId(), user);
+    return ApiResult.OK(true);
+  }
+
+  @GetMapping(path = "/{userId}")
+  public ApiResult<User> get(@PathVariable(value = "userId") String id, HttpSession httpSession) {
+    User sessionUser = (User) Optional.ofNullable(httpSession.getAttribute(id)).
+        orElseThrow(() -> new UnauthorizedException(id));
+    User user = userService.findById(id).orElseThrow(() -> new UserNotFoundException(id));
+
+    if (!sessionUser.equals(user)) {
+      return ApiResult.ERROR("잘못된 접근입니다", HttpStatus.UNAUTHORIZED);
+    }
+    return ApiResult.OK(user);
   }
 }
