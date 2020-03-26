d9d9c587e99b6ed8af0f3ac1570b583f9880fb4f
ehgud0670 <kimdo2297@gmail.com>
2020-03-26 16:09:57 +0900

    [refactor] 텍스트필드의 텍스트 시작 위치 설정 및 텍스트 크기 줄임

--- iOS/SignupApp/SignupApp/Views/TextField/CustomTextField.swift
+++ iOS/SignupApp/SignupApp/Views/TextField/CustomTextField.swift
@@ -26,6 +26,22 @@ class CustomTextField: UITextField {
     }
 }
 
+extension CustomTextField {
+    private static let padding = UIEdgeInsets(top: 0, left: 10, bottom: 0, right: 0)
+    
+    override func textRect(forBounds bounds: CGRect) -> CGRect {
+        return bounds.inset(by: CustomTextField.padding)
+    }
+    
+    override func placeholderRect(forBounds bounds: CGRect) -> CGRect {
+        return bounds.inset(by: CustomTextField.padding)
+    }
+    
+    override func editingRect(forBounds bounds: CGRect) -> CGRect {
+        return bounds.inset(by: CustomTextField.padding)
+    }
+}
+
 extension UIColor {
     convenience init?(red: Int, green: Int, blue: Int) {
         guard red >= 0 && red <= 255,
