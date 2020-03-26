3730e2fe3d7f0167322a28ed6a59fed8a27fba21
Han <quaint3@gmail.com>
2020-03-26 13:16:43 +0900

    [refactor] 헬스체크 컨트롤러 swagger 추가
    app 헬스 컨트롤러에 swagger를 추가하고, 정상적으로 작동하는지 확인.

--- BE/signup-server/src/main/java/com/signup11/be/controller/HealthCheckRestController.java
+++ BE/signup-server/src/main/java/com/signup11/be/controller/HealthCheckRestController.java
@@ -1,15 +1,18 @@
 package com.signup11.be.controller;
 
+import com.signup11.be.api.ApiResult;
+import io.swagger.annotations.Api;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("api/_hcheck")
+@Api(tags = "헬스체크 API")
 public class HealthCheckRestController {
 
   @GetMapping
-  public Long healthCheck() {
-    return System.currentTimeMillis();
+  public ApiResult<Long> healthCheck() {
+    return ApiResult.OK(System.currentTimeMillis());
   }
 }
