c28c6414a379b065bf4f2457c82905dfd1bf2c67
ehgud0670 <kimdo2297@gmail.com>
2020-03-26 23:19:47 +0900

    [feat] 비밀번호 유효성을 검사하고 메시지를 출력하는 기능 구현

--- iOS/SignupApp/SignupApp/Delegates/PWTextFieldDelegate.swift
+++ iOS/SignupApp/SignupApp/Delegates/PWTextFieldDelegate.swift
@@ -12,4 +12,24 @@ class PWTextFieldDelegate: CustomTextFieldDelegate {
     override var textLimit: Int {
         return 16
     }
+    
+    override func textFieldShouldEndEditing(_ textField: UITextField) -> Bool {
+        if let pwTextField = textField as? PWTextField {
+            if !Controller.isTextCorrectLength(min: 8,
+                                               max: 16,
+                                               count: textField.text?.count) {
+                pwTextField.setMessageCorrectTextLength()
+            } else if !Controller.hasUpperCaseLetter(textField.text) {
+                pwTextField.setMessageWriteUpperCapitalLetter()
+            } else if !Controller.hasNumber(textField.text) {
+                pwTextField.setMessageWriteNumber()
+            } else if !Controller.hasSpecialCharacter(textField.text) {
+                pwTextField.setMessageWriteSpecialCharacter()
+            } else {
+                pwTextField.setMessageCorrectPassword()
+                return true
+            }
+        }
+        return super.textFieldShouldEndEditing(textField)
+    }
 }
--- iOS/SignupApp/SignupApp/Views/TextFields/PWTextField.swift
+++ iOS/SignupApp/SignupApp/Views/TextFields/PWTextField.swift
@@ -9,4 +9,38 @@
 import UIKit
 
 final class PWTextField: CustomTextField {
+    func setMessageCorrectTextLength() {
+        let messageCorrectTextLength = "8자 이상 16자 이하로 입력해주세요."
+        messageLabel.text = messageCorrectTextLength
+        messageLabel.textColor = UIColor.redColor
+        messageLabel.isHidden = false
+    }
+    
+    func setMessageWriteUpperCapitalLetter() {
+        let messageWriteUpperCapitalLetter = "영문 대문자를 최소 1자 이상 포함해주세요. "
+        messageLabel.text = messageWriteUpperCapitalLetter
+        messageLabel.textColor = UIColor.redColor
+        messageLabel.isHidden = false
+    }
+    
+    func setMessageWriteNumber() {
+        let messageWriteNumber = "숫자를 최소 1자 이상 포함해주세요."
+        messageLabel.text = messageWriteNumber
+        messageLabel.textColor = UIColor.redColor
+        messageLabel.isHidden = false
+    }
+    
+    func setMessageWriteSpecialCharacter() {
+        let messageWriteSpecialCharacter = "특수문자를 최소 1자 이상 포함해주세요.(!@#$%)"
+        messageLabel.text = messageWriteSpecialCharacter
+        messageLabel.textColor = UIColor.redColor
+        messageLabel.isHidden = false
+    }
+    
+    func setMessageCorrectPassword() {
+        let messageCorrectPassword = "안전한 비밀번호입니다."
+        messageLabel.text = messageCorrectPassword
+        messageLabel.textColor = UIColor.greenColor
+        messageLabel.isHidden = false
+    }
 }
