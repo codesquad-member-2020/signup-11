81cfc8c6e1102f2d2b96b11bdab57e1e31330e1a
Han <quaint3@gmail.com>
2020-03-27 11:44:14 +0900

    [fix] Unauthorized Exception 수정
    id만 받지 않으니, 에러 메세지를 받도록 우선 함

--- BE/signup-server/src/main/java/com/signup11/be/error/UnauthorizedException.java
+++ BE/signup-server/src/main/java/com/signup11/be/error/UnauthorizedException.java
@@ -2,7 +2,7 @@ package com.signup11.be.error;
 
 public class UnauthorizedException extends RuntimeException {
 
-  public UnauthorizedException(String userId) {
-    super("UnauthorizedException User: " + userId);
+  public UnauthorizedException(String errorMessage) {
+    super("UnauthorizedException User: " + errorMessage);
   }
 }
