da0c90843eac3036e4fca832bd568b06b86cf71f
Han <quaint3@gmail.com>
2020-03-27 12:04:33 +0900

    [fix] swagger 예시 수정, data 추가

--- BE/signup-server/src/main/resources/data.sql
+++ BE/signup-server/src/main/resources/data.sql
@@ -1,3 +1,4 @@
 INSERT INTO users (seq,id,password,email,name,birth,gender,phone) VALUES (null, 'test', '1234', 'test@gmail.com', '테스터', '2020-03-22','남', '010-1234-5678');
+INSERT INTO users (seq,id,password,email,name,birth,gender,phone) VALUES (null, 'codesquad', '1234', 'codesuqad@gmail.com', '코드스쿼', '1999-03-22','남', '010-0987-1234');
 
 INSERT INTO interests (seq, user_seq, content) VALUES (null, 1  , '영화');
