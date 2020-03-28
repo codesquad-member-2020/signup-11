e3a35f61f6fba1af4c7dc68b95cf61ed92e9dde9
ehgud0670 <kimdo2297@gmail.com>
2020-03-28 23:53:25 +0900

    [feat] id 텍스트 입력할때마다 검증하도록 기능 구현함

--- iOS/SignupApp/SignupApp/Delegates/IDTextFieldDelegate.swift
+++ iOS/SignupApp/SignupApp/Delegates/IDTextFieldDelegate.swift
@@ -9,6 +9,21 @@
 import UIKit
 
 final class IDTextFieldDelegate: SignupTextFieldDelegate {
+    override func textFieldDidChangeSelection(_ textField: UITextField) {
+        guard let idTextField = textField as? IDTextField else { return }
+        if !Controller.isCorrectID(idTextField.text) {
+            idTextField.setWrongCaseByWrongID()
+        } else {
+            Controller.isOverlappedID(idTextField.text) { (result) in
+                guard let result = result else { return }
+                DispatchQueue.main.async {
+                    result ? idTextField.setWrongCaseByOverlappedID() : idTextField.setCorrectCase()
+                }
+            }
+        }
+        super.textFieldDidChangeSelection(textField)
+    }
+    
     override func textFieldShouldEndEditing(_ textField: UITextField) -> Bool {
         if let idTextField = textField as? IDTextField {
             if !Controller.isCorrectID(idTextField.text) {
