7a3babb19731822b4666e51c6f0ca141ee86ee7a
ehgud0670 <kimdo2297@gmail.com>
2020-03-28 23:54:58 +0900

    [feat] 비밀번호 텍스트 입력할때마다 유효성 검사하도록 기능 구현함

--- iOS/SignupApp/SignupApp/Delegates/PWTextFieldDelegate.swift
+++ iOS/SignupApp/SignupApp/Delegates/PWTextFieldDelegate.swift
@@ -13,6 +13,24 @@ class PWTextFieldDelegate: SignupTextFieldDelegate {
         return 16
     }
     
+    override func textFieldDidChangeSelection(_ textField: UITextField) {
+        guard let pwTextField = textField as? PWTextField else { return }
+        if !Controller.isTextCorrectLength(min: 8,
+                                           max: 16,
+                                           count: pwTextField.text?.count) {
+            pwTextField.setWrongCaseByUnsuitableTextLength()
+        } else if !Controller.hasUpperCaseLetter(pwTextField.text) {
+            pwTextField.setWrongCaseByNoUpperCapitalLetter()
+        } else if !Controller.hasNumber(pwTextField.text) {
+            pwTextField.setWrongCaseByNoNumber()
+        } else if !Controller.hasSpecialCharacter(pwTextField.text) {
+            pwTextField.setWrongCaseByNoSpecialCharacter()
+        } else {
+            pwTextField.setCorrectCase()
+        }
+        super.textFieldDidChangeSelection(textField)
+    }
+    
     override func textFieldShouldEndEditing(_ textField: UITextField) -> Bool {
         if let pwTextField = textField as? PWTextField {
             if !Controller.isTextCorrectLength(min: 8,
